7f013710dea13cea4fb062a3d87505aeca94ea03
Danilo Herrera <danilo@agileninja.io>
2025-02-15 23:28:04 -0500

    Update vim options

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -4,3 +4,9 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number relativenumber")
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<A-j>", ":cprev<CR>", { silent = true })
+vim.keymap.set("n", "<A-k>", ":cnext<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>p", '"_dP')
+vim.keymap.set("n", "<leader>y", '"+y')
