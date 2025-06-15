0ddc48f32d14c7d4e1388237ccea299fdd46c28c
Danilo Herrera <danilo@agileninja.io>
2025-06-14 20:55:18 -0400

    Fix trouble.nvim config

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -3,6 +3,39 @@ return {
 	dependencies = {
 		"kyazdani42/nvim-web-devicons",
 	},
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
 	config = function()
 		require("trouble").setup({
 			auto_open = false,
@@ -18,21 +51,5 @@ return {
 			},
 			use_diagnostic_signs = true,
 		})
-
-		vim.keymap.set("n", "<leader>xx", function()
-			require("trouble").toggle()
-		end)
-		vim.keymap.set("n", "<leader>xw", function()
-			require("trouble").toggle("workspace_diagnostics")
-		end)
-		vim.keymap.set("n", "<leader>xd", function()
-			require("trouble").toggle("document_diagnostics")
-		end)
-		vim.keymap.set("n", "<leader>xq", function()
-			require("trouble").toggle("quickfix")
-		end)
-		vim.keymap.set("n", "<leader>xl", function()
-			require("trouble").toggle("loclist")
-		end)
 	end,
 }
